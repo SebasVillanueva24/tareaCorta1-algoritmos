abc044ea2a9d922b135eba517c5844eebbe1cec6
Sebastián Villanueva <elsebuchas@hotmail.com>
2020-02-23 01:00:46 -0600

    Algoritmos de busqueda
    Añadí ambos algoritmos a la carpeta resources del proyecto por si acaso

---
+++ Tarea Corta 1 Sebastian Villanueva/Assets/Resources/BubbleSortScript.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BubbleSortScript : MonoBehaviour
+{
+    void BubbleSort(int[] listaSinOrdenar)
+    {
+        int[] arr = { 78, 55, 45, 98, 13 };
+        int temp;
+        for (int j = 0; j <= listaSinOrdenar.Length - 2; j++) {
+            for (int i = 0; i <= listaSinOrdenar.Length - 2; i++) {
+                if (listaSinOrdenar[i] > listaSinOrdenar[i + 1]) {
+                    temp= listaSinOrdenar[i + 1];
+                    listaSinOrdenar[i + 1] = listaSinOrdenar[i];
+                    listaSinOrdenar[i] = temp;
+                    ImprimirLista(listaSinOrdenar);
+                }
+            }
+        }
+
+        string resultado = "";
+        for (int i = 0; i < listaSinOrdenar.Length; i++)
+        {
+            resultado = resultado + listaSinOrdenar[i] + ", ";
+        }
+        
+        print(resultado);
+    }
+    
+    void ImprimirLista(int[] algunaLista)
+    {
+        string resultado = "";
+        for (int i = 0; i < algunaLista.Length; i++)
+        {
+            resultado = resultado + algunaLista[i] + ", ";
+        }
+        
+        print(resultado);
+    }
+
+    int[] generarLista(int tamanno)
+    {
+        Random r = new Random();
+        int[] arr = new int[tamanno];
+
+        for (int i = 0; i < tamanno; i++)
+        {
+            arr[i] = Random.Range(1,tamanno+1);
+        }
+
+        return arr;
+
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        
+        int[] lista = generarLista(90);
+        ImprimirLista(lista);
+        BubbleSort(lista);
+        
+        watch.Stop();
+        
+        print($"Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Tarea Corta 1 Sebastian Villanueva/Assets/Resources/SelectionSortScript.cs
@@ -0,0 +1,77 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelectionSortScript : MonoBehaviour
+{
+    void SelectionSort(int[] listaSinOrdenar)
+    {
+        int min;
+        int temp;
+        
+        for (int i = 0; i < listaSinOrdenar.Length; i++)
+        {
+            min = i;
+            
+            for(int j = i+1; j < listaSinOrdenar.Length; j++)
+            {
+                if(listaSinOrdenar[j] < listaSinOrdenar[min])
+                {
+                    min = j;
+                }
+            }
+            
+            if(min != i)
+            {
+                temp = listaSinOrdenar[i];
+                listaSinOrdenar[i] = listaSinOrdenar[min];
+                listaSinOrdenar[min] = temp;
+                ImprimirLista(listaSinOrdenar);
+            }
+        }
+    }
+    
+    void ImprimirLista(int[] algunaLista)
+    {
+        string resultado = "";
+        for (int i = 0; i < algunaLista.Length; i++)
+        {
+            resultado = resultado + algunaLista[i] + ", ";
+        }
+        
+        print(resultado);
+    }
+
+    int[] generarLista(int tamanno)
+    {
+        Random r = new Random();
+        int[] arr = new int[tamanno];
+
+        for (int i = 0; i < tamanno; i++)
+        {
+            arr[i] = Random.Range(1,tamanno+1);
+        }
+
+        return arr;
+
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        var watch = System.Diagnostics.Stopwatch.StartNew();
+        
+        int[] lista = generarLista(100);
+        ImprimirLista(lista);
+        SelectionSort(lista);
+        
+        watch.Stop();
+        
+        print($"Execution Time: {watch.ElapsedMilliseconds} ms");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
